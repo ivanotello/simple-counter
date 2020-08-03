4240c72a89caa64ab808964bcd08e7a22e051163
ivanotello <iodaruosgg@gmail.com>
2020-08-03 18:44:44 +0000

    Simple counter completado

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,47 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const containerStyle = {
+	fontSize: "60px",
+	color: "whitesmoke",
+	background: "#101010",
+	padding: "20px"
+};
+const numberStyle = {
+	background: "#323232",
+	padding: "20px",
+	margin: "20px"
+};
 
-//create your first component
-export function Home() {
+const SimpleCounter = props => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div
+			className="container d-flex flex-row justify-content-center"
+			style={containerStyle}>
+			<div className="clock" style={numberStyle}>
+				<i className="far fa-clock" />
+			</div>
+			<div className="number-four" style={numberStyle}>
+				{props.numberFour % 10}
+			</div>
+			<div className="number-three" style={numberStyle}>
+				{props.numberThree % 10}
+			</div>
+			<div className="number-two" style={numberStyle}>
+				{props.numberTwo % 10}
+			</div>
+			<div className="number-one" style={numberStyle}>
+				{props.numberOne % 10}
+			</div>
 		</div>
 	);
-}
+};
+
+SimpleCounter.propTypes = {
+	numberFour: PropTypes.number,
+	numberThree: PropTypes.number,
+	numberTwo: PropTypes.number,
+	numberOne: PropTypes.number
+};
+
+export default SimpleCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,25 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-//include bootstrap npm library into the bundle
 import "bootstrap";
-
-//include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
+import SimpleCounter from "./component/home.js";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function() {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	counter++;
+
+	ReactDOM.render(
+		<SimpleCounter
+			numberFour={four}
+			numberThree={three}
+			numberTwo={two}
+			numberOne={one}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
